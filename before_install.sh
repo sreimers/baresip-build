3d7b1826364269fb7e577fa18be3f841a6ec58ab
Sebastian Reimers <info@miete-admin.de>
2015-11-09 22:45:39 +0100

    fix sudo bug

--- before_install.sh
+++ before_install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-set -e
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-    sudo apt-get update -qq
-    sudo apt-get install -y libssl-dev
-    sudo rm -f /usr/lib/x86_64-linux-gnu/libcrypto.so
-    sudo rm -f /usr/lib/x86_64-linux-gnu/libssl.so
+    apt-get update -qq
+    apt-get install -y libssl-dev
+    rm -f /usr/lib/x86_64-linux-gnu/libcrypto.so
+    rm -f /usr/lib/x86_64-linux-gnu/libssl.so
 fi
