fe5ad5395ab0154e201f34c61a1ee4b17008f3fb
Sebastian Reimers <info@miete-admin.de>
2015-11-09 18:46:15 +0100

    combine osx and linux build

--- build.sh
+++ build.sh
@@ -5,6 +5,15 @@ re="0.4.14"
 opus="1.1"
 baresip="0.4.15"
 
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+    my_extra_lflags=""
+else
+    my_extra_lflags="-framework SystemConfiguration -framework CoreFoundation"
+fi
+
+-framework SystemConfiguration -framework CoreFoundation 
+
+
 rm -Rf baresip-$baresip re rem re-$re rem-$rem opus-$opus
 
 wget -N "http://www.creytiv.com/pub/re-0.4.14.tar.gz"
@@ -27,7 +36,15 @@ cp opus-$opus/include/*.h my_include/opus/
 wget -N "http://www.creytiv.com/pub/baresip-0.4.15.tar.gz"
 tar -xzf baresip-${baresip}.tar.gz
 cd baresip-$baresip;
-make LIBRE_SO=../re LIBREM_PATH=../rem STATIC=1 MODULES="opus stdio ice menu g711 turn stun uuid auloop contact" EXTRA_CFLAGS="-I ../my_include" EXTRA_LFLAGS="-L ../opus"
+
+make LIBRE_SO=../re LIBREM_PATH=../rem STATIC=1 \
+    MODULES="opus stdio ice menu g711 turn stun uuid auloop contact" \
+    EXTRA_CFLAGS="-I ../my_include" EXTRA_LFLAGS="$my_extra_lflags -L ../opus"
 cd ..
 
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
 ldd baresip-$baresip/baresip
+else
+otool -L baresip-$baresip/baresip
+fi
+baresip-$baresip/baresip -t
